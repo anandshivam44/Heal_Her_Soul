a836ec90fe503f60dfe9c34f9cc9a50338693934
victorhez <victorhezishere@gmail.com>
2020-04-13 23:33:49 +0100

    Second Update

--- app/src/main/java/com/example/healhersoul/IntroPage.java
+++ app/src/main/java/com/example/healhersoul/IntroPage.java
@@ -2,7 +2,10 @@ package com.example.healhersoul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
@@ -10,6 +13,7 @@ import com.synnapps.carouselview.ImageListener;
 
 public class IntroPage extends AppCompatActivity {
    private CarouselView carouselView;
+   private Button next;
    private int[] sampleImages = {R.drawable.dr, R.drawable.group2, R.drawable.happy3, R.drawable.birthrate, R.drawable.pregnantfirst
            , R.drawable.thebabyscan, R.drawable.pregnanter, R.drawable.pregnantmom, R.drawable.womanpregnant};
     @Override
@@ -19,7 +23,13 @@ public class IntroPage extends AppCompatActivity {
         carouselView = findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
-
+        next=findViewById(R.id.nextHomeBtn);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroPage.this, LoginPage.class));
+            }
+        });
     }
     ImageListener imageListener = new ImageListener() {
         @Override
