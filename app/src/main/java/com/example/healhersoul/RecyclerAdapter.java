daf7c103c730526f0324fbadabb55042fed37f64
Anubhaw19 <anubhawsharma99@gmail.com>
2020-05-12 13:10:07 +0530

    RecylerView for Article

---
+++ app/src/main/java/com/example/healhersoul/RecyclerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.healhersoul;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter <RecyclerAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<Data> data;
+    public RecyclerAdapter(Context context, List<Data> data)
+    {
+        this.context=context;
+        this.data=data;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+       View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_list,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Data currentData=data.get(position);
+        Glide.with(context).load(currentData.getImage_url()).into(holder.image); //Glide library for loading image
+        holder.title.setText(currentData.getNews_title());
+        holder.description.setText(currentData.getNews_description());
+        holder.content.setText(currentData.getContent());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView title,description,content;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image=itemView.findViewById(R.id.newsImg);
+            title=itemView.findViewById(R.id.txt_title);
+            description=itemView.findViewById(R.id.txt_description);
+            content=itemView.findViewById(R.id.txt_content);
+        }
+    }
+}
