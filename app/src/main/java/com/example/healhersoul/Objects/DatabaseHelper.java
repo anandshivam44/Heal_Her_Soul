48a039dc55faefca7481c06207c37871072c7e40
Anubhaw19 <anubhawsharma99@gmail.com>
2020-05-24 00:05:16 +0530

    name and phone number stored in SQLite Database

--- app/src/main/java/com/example/healhersoul/Objects/DatabaseHelper.java
+++ app/src/main/java/com/example/healhersoul/Objects/DatabaseHelper.java
@@ -11,11 +11,13 @@ import androidx.annotation.Nullable;
 import java.util.Date;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static  final  String DATABASE_NAME="women.db";
-    public static  final  String TABLE_NAME="women_table";
+    public static  final  String DATABASE_NAME="patient.db";
+    public static  final  String TABLE_NAME="patient_table";
     public static  final  String COL_1="Date";
     public static  final  String COL_2="Month";
     public static  final  String COL_3="Year";
+    public static  final  String COL_4="Name";
+    public static  final  String COL_5="Phone";
 
 
 
@@ -27,7 +29,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table "+TABLE_NAME+"(DATE INTEGER ,MONTH INTEGER,YEAR INTEGER)");
+        db.execSQL("create table "+TABLE_NAME+"(DATE INTEGER ,MONTH INTEGER,YEAR INTEGER,NAME TEXT,PHONE TEXT)");
         // this will execute our SQL statement & create a table.
     }
 
@@ -37,7 +39,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);// this  is again calling onCreate method ,to create a new table ,after deleting the previously existing table.
     }
 
-    public boolean insertData(Integer date,Integer month,Integer year) // this method is used to insert data in table.
+    public boolean insertData(Integer date,Integer month,Integer year,String name,String phone) // this method is used to insert data in table.
     {
         SQLiteDatabase db=this.getWritableDatabase();// creating SQLite database Object, for getting writable database.
 
@@ -47,6 +49,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(COL_1, date);
         contentValues.put(COL_2,month);
         contentValues.put(COL_3,year);
+        contentValues.put(COL_4,name);
+        contentValues.put(COL_5,phone);
         long result= db.insert(TABLE_NAME,null,contentValues);
         if(result==-1)
             return false;
